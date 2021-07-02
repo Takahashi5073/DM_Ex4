99d5081915f983679c6d79194c3116f51f8a4261
Takahashi5073 <86813041+Takahashi5073@users.noreply.github.com>
2021-07-02 11:27:16 +0900

    最初のバージョン

---
+++ ex4.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+class DM_Ex4{
+    public static void main (String [] argc){
+
+        Random num = new Random();
+        int value1, value2, total;
+
+        value1 = num.nextInt(6) +1;
+        value2 = num.nextInt(6) +1;
+        total = value1 + value2;
+
+        System.out.println("Die 1:" +value1);
+        System.out.println("Die 2:" +value2);
+        System.out.println("Total Value:" +total);
+
+        
+
+
+
+    }
+}
