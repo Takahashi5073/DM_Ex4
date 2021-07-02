cb9d6bdf8c230b7778742bd5247eb7c212503033
Takahashi5073 <86813041+Takahashi5073@users.noreply.github.com>
2021-07-02 11:48:49 +0900

    User name added

--- ex4.java
+++ ex4.java
@@ -1,7 +1,16 @@
 import java.util.Random;
+import java.util.Scanner;
 
-class DM_Ex4{
+class ex4{
     public static void main (String [] argc){
+
+    //add_part
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = scanner.next();
+    System.out.println("Hello, "+name+"!");
+
+    //dice_part
 
         Random num = new Random();
         int value1, value2, total;
@@ -13,10 +22,6 @@ class DM_Ex4{
         System.out.println("Die 1:" +value1);
         System.out.println("Die 2:" +value2);
         System.out.println("Total Value:" +total);
-
-        
-
-
 
     }
 }
